8bfc9487ec76d47d1b9f4e62b8204ab02e079ee9
Luca Arienti <lucarienti@gmail.com>
2022-06-20 22:50:56 +0200

    fix order by created_at articles in home

--- components/Home/Blog.vue
+++ components/Home/Blog.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="home-portfolio container mx-auto justify-center flex flex-col py-16 lg:py-20 px-6 xl:px-0 lg:max-w-7xl">
-    <section-title class="self-center" light text="Articoli" />
+  <section
+    class="home-portfolio container mx-auto justify-center flex flex-col py-16 lg:py-20 px-6 xl:px-0 lg:max-w-7xl"
+  >
+    <section-title class="self-center" light text="Articoli"/>
     <h3 class="mt-4 text-center font-title text-3xl lg:text-5xl font-black text-brown-default">
       Scopri tutte le novità dal mondo make-up
     </h3>
@@ -29,7 +31,11 @@ export default {
   },
 
   async fetch () {
-    this.articles = await this.$content('articles').where({ isPublished: true }).limit(3).fetch()
+    this.articles = await this.$content('articles')
+      .where({ isPublished: true })
+      .sortBy('createdAt', 'desc')
+      .limit(3)
+      .fetch()
   }
 }
 </script>
